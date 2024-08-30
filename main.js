2a21d0be4d7907e52c63d40b3927267a5e517945
missesenam <moneyesenam@gmail.com>
2024-08-30 13:05:02 +0100

    done with the calc app

---
+++ main.js
@@ -0,0 +1,31 @@
+const display = document.getElementById('inputfield');
+const result = document.getElementById('showresult');
+// display value
+function displayval(val){
+    display.value += val;
+}
+// clear all
+function clearAll(){
+    display.value = '';
+    showresult.value = '';
+}
+// delete
+function deleteElements(){
+    display.value = display.value.toString().slice(0,-1);
+    showresult.value = '';
+}
+// calculate
+function calculate(){
+    // if (display.value == ''){
+    //     showresult.value = '' 
+    // }else{
+    // showresult.value = eval(display.value)
+    // }
+    try{
+        showresult.value = eval(display.value)
+    }
+    catch{
+        showresult.value = 'error'
+        alert('put smtg u airhaid')
+    }
+}
